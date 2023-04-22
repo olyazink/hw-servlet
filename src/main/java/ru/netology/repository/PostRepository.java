7fb87eb211919c3a6414b2e27d152f0e87827fd3
OlgaTsinkevich <olyazink@gmail.com>
2023-04-22 20:56:21 +0300

    Second commit

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -7,34 +7,35 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
 public class PostRepository {
-  private ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
-  private long idCounter;
+    private ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+    private AtomicLong idCounter = new AtomicLong(1);
 
-  public List<Post> all() {
-    return new ArrayList<>(posts.values());
-  }
-
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(posts.get(id));
-  }
-
-  public Optional<Post> save(Post post) {
-    long postId = post.getId();
-    if (postId == 0) {
-      long newPostId = idCounter+1;
-      post.setId(newPostId);
-      posts.put(newPostId, post);
-      return Optional.of(posts.get(newPostId));
-    } else {
-      if (posts.containsKey(postId)) posts.put(postId, post);
-      return Optional.ofNullable(posts.get(postId));
+    public List<Post> all() {
+        return new ArrayList<>(posts.values());
     }
-  }
 
-  public Optional<Post> removeById(long id) {
-    return Optional.ofNullable(posts.remove(id));
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Optional<Post> save(Post post) {
+        long postId = post.getId();
+        if (postId == 0) {
+            long newPostId = idCounter.getAndIncrement();
+            post.setId(newPostId);
+            posts.put(newPostId, post);
+            return Optional.of(posts.get(newPostId));
+        } else {
+            if (posts.containsKey(postId)) posts.put(postId, post);
+            return Optional.ofNullable(posts.get(postId));
+        }
+    }
+
+    public Optional<Post> removeById(long id) {
+        return Optional.ofNullable(posts.remove(id));
+    }
 }
